e11c4a8285624a87e2b9c736fd5d447e5628c494
NatalieLouise91 <natalie936@hotmail.com>
2021-10-20 14:56:15 +1000

    added json file

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 function Landing() {
+
+
+
+    // fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699")
+    // .then(res =>res.json())
+    // .then(({ items: [ { volumeInfo : { title, description} } ] }) => {
+    //   console.log(title);
+    //   console.log(description);
+    // }),
+    // error => {
+    //   console.log(error + '');
+    // };
+    
+
   return (
     <div>
         <h1>Landing</h1>
