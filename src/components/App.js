7bc8b8c770e35709fcb60c83152e4fa5c7036bbc
NatalieLouise91 <natalie936@hotmail.com>
2021-10-20 12:46:10 +1000

    added about, categories, contacts and navigation links

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import '../App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Landing from './Landing';
 import Navigation from './Navigation';
+import About from './About';
+import Categories from './Categories';
+import Contacts from './Contacts';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navigation/>
         <Switch>
           <Route path="/" exact component={Landing}/>
+          <Route path="/about" exact component={About}/>
+          <Route path="/categories" exact component={Categories}/>
+          <Route path="/contacts" exact component={Contacts}/>
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,10 +3,50 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
     return (
-      <div>   
-            <Link to="/">Home</Link>        
-      </div>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+        <div className="container">
+            <a href="/" className="navbar-brand black">Booktrace</a>
+            <button
+                type="button" 
+                className="navbar-toggler"
+                data-bs-target="#navbarNav"
+                data-bs-toggle="collapse"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle Navbar"
+                >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+      
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <div className="mx-auto"></div>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to ="/" className="nav-link text-black">
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to ="/about" className="nav-link text-black">
+                        About
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to ="/categories" className="nav-link text-black">
+                        Categories
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to ="/contacts" className="nav-link text-black">
+                        Contacts
+                    </Link>
+                </li>
+            </ul>
+        </div>
+        </div>
+      </nav>
     );
   }
   
   export default Navigation;
+
