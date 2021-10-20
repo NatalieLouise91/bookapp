56c141cf11e982db8c1a6293850a8f896ce323b2
NatalieLouise91 <natalie936@hotmail.com>
2021-10-20 12:06:20 +1000

    added basic structure and react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,18 @@
-import logo from '../logo.svg';
+import React from 'react';
 import '../App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './Landing';
+import Navigation from './Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navigation/>
+        <Switch>
+          <Route path="/" exact component={Landing}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
