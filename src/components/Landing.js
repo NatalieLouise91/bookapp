927cac822ad281617f6941d512e9f17de24c98b9
NatalieLouise91 <natalie936@hotmail.com>
2021-10-25 15:15:20 +1000

    added search functionality for searchbar

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,42 +1,20 @@
-import React, { useEffect, useState } from 'react';
-
+import React from 'react';
+import axios from 'axios'; 
 
 function Landing() {
-  const [title,setTitle] = useState("")
-  const [author,setAuthor] = useState("")
-  const [description, setDescription] = useState("")
-  const [image, setImage] = useState("")
 
-useEffect(() => {
-
-  const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699";
-  
-  const fetchData = async () => {
-    try {
-        const response = await fetch(url);
-        const json = await response.json();
-        setTitle(json.items[0].volumeInfo.title);
-        setAuthor(json.items[0].volumeInfo.authors);
-        setDescription(json.items[0].volumeInfo.description);
-        setImage(json.items[0].volumeInfo.imageLinks.thumbnail);
-    } catch (error) {
-        console.log("error",error);
+    function topBooks() {  
+        const book = 5
+        axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=&maxResults=10")  
+            .then(data => {  
+                console.log(data.data.items);  
+            })  
     }
-  };
-
-  fetchData();
-  }, []);
-
-    return (
-      <div>
-          <h1>{title}</h1>
-          <h1>{author}</h1>
-          <p>{description}</p>
-          <img src={image}/>
-      </div>
-    );
-
-
+    return(
+        <div>
+            <p>{topBooks}</p>   
+        </div>
+    )
 }
 
 export default Landing;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,56 @@
 import React, { useState } from 'react';
+import axios from 'axios'; 
 
-const SearchBar = () => {
+const SearchBar = () => {  
+    const [book, setBook] = useState("");  
+    const [result, setResult] = useState([]);   
   
-    const [search, setSearch] = useState([])
-    console.log(setSearch)
+    function handleChange(event) {  
+        const book = event.target.value;  
+        setBook(book);  
+    }  
+    function handleSubmit(event) {  
+        event.preventDefault();  
+        axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=&maxResults=40")  
+            .then(data => {  
+                console.log(data.data.items);  
+                setResult(data.data.items);  
+            })  
+    }
+
     return (
-    <div className="searchbar">
-        <form action="">
-            <input type ="text" 
-            search={search}
-            onChange={event => setSearch(event.target.search)}/>
-            <button type="submit" >Search</button>
+        <form onSubmit={handleSubmit}>  
+        <div className="card-header main-search">  
+            <div className="row">  
+                <div className="col-12 col-md-3 col-xl-3">  
+                    <input onChange={handleChange} className="AutoFocus form-control" placeholder="Type something..." type="text" />  
+                </div>  
+                <div className="ml-auto">  
+                    <input type="submit" value="Search" className="btn btn-primary search-btn" />  
+                </div>  
+            </div>  
+        </div>
+
+        <div className="container">  
+                <div className="row">  
+                    {result.map(book => (  
+                        <div className="col-sm-2">  
+                            <div style={{ 'marginTop': '10px' }}>  
+  
+                                <img variant="top" src={book.volumeInfo.imageLinks !== undefined ? book.volumeInfo.imageLinks.thumbnail : ''} alt={book.title} />  
+                                <div>  
+                                    <h5 className="card-title">{book.volumeInfo.title}</h5>
+                                    <p className="card-title">{book.volumeInfo.authors}</p> 
+                                    <p className="card-title">{book.volumeInfo.averageRating}</p>   
+                                    <a href= {book.volumeInfo.previewLink} target="_blank" className="btn btn-primary" >Preview</a> 
+                                </div>  
+                            </div>  
+                        </div>  
+                    ))}  
+                </div>  
+            </div>
         </form>
-    </div>
-  );
+    )
 }
 
 export default SearchBar;
