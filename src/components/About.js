06e8606f465fc06cb2e45f47105a30b857de9e1f
Russell Moore <contactrussellmoore@gmail.com>
2021-10-25 16:03:54 +1000

    Added infotext

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,9 @@ function About() {
   return (
     <div>
         <h1>About</h1>
+        <p>BookMark is an app that allows users (that's you!) to find books based upon keywords, book titles or author names!</p>
+        <p>It will deliver The book title, Author name and the cover image for all results delivered from your serch query.</p>
+        <p>A handy link is provided to purchase a title, or view the work on the Google Books.</p>
     </div>
   );
 }
