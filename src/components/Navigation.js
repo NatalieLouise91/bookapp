6ec99bc9e2aa438842dd28820d8a954bf67ceb6f
NatalieLouise91 <natalie936@hotmail.com>
2021-10-26 11:50:00 +1000

    fixed navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "../Navbar.css"
+import SearchBar from './SearchBar'
 
 function Navigation() {
     
     return (
-    <header>
+    <header>  
         <nav className="navbar">
                 <div className="container links row mx-auto">
                 <Link to="/" className="nav-link col">Home</Link>
                 <Link to="/about" className="nav-link col">About</Link>
-                <Link to="/categories" className="nav-link col">Categories</Link>
+                {/* <Link to="/categories" className="nav-link col">Categories</Link> */}
                 <Link to="/contacts" className="nav-link col ">Contacts</Link>
                 {/* <Link to="/preview">Preview</Link> */}
             </div>
-        </nav>
+        </nav> 
+        <SearchBar/> 
     </header>
     );
   }
