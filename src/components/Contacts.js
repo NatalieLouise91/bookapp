c713baa7d5e78d9febd3ea73283fbffbb566ac12
benaitcheson <benaitcheson@hotmail.com>
2021-10-26 10:19:07 +1000

    added more text to contacts page

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -5,6 +5,7 @@ function Contacts() {
   return (
     <div>
         <h1>Contact</h1>
+        <p>We are a team of Coders from Coder Academy. This site is our second Hackathon project.</p>
         <p>Here is the Github repo for this project. You can reach us here to raise any issues or modifications.</p>
         <a href="https://github.com/NatalieLouise91/bookapp"><img src={Github} alt="link to github" height="60px"/></a>
     </div>
