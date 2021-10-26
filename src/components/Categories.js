9bee98392010067d693ce05e69b696e2414a1381
benaitcheson <benaitcheson@hotmail.com>
2021-10-26 11:38:10 +1000

    added categories text

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+const catDropDown = ["one"]
+
+
 function Categories() {
   return (
     <div>
         <h1>Categories</h1>
+
+        <p>Drop menu for categories from google books api</p>
+
+        
     </div>
   );
 }
