0ef4898d88ad083b2ec4fed2baa9cbd589027b78
NatalieLouise91 <natalie936@hotmail.com>
2021-10-26 13:12:17 +1000

    changed styling for about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import '../App.css';
 
 function About() {
   return (
-    <div>
-        <h1>About</h1>
-        <p>BookMark is an app that allows users (that's you!) to find books based upon keywords, book titles or author names!</p>
-        <p>It will deliver The book title, Author name and the cover image for all results delivered from your serch query.</p>
-        <p>A handy link is provided to purchase a title, or view the work on the Google Books.</p>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4 text-center font-weight-bold">About</h1>
+        <hr class="my-4 bold"/>
+        <p class="text-center">BookMark is an app that allows users (that's you!) to find books based upon keywords, book titles or author names!</p>
+        <br/>
+        <p class="text-center">It will deliver The book title, author name and the cover image for all results delivered from your serch query.</p>
+        <br/>
+        <p class="text-center">A handy link is provided to purchase a title, or view the work on the Google Books.</p>
+      </div>
     </div>
   );
 }
